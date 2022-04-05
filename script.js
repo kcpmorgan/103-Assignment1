604f8f16f7d115ca8f11fae4fed2ba20663ff8f5
Kim Morgan <101612445+kcpmorgan@users.noreply.github.com>
2022-04-04 23:06:53 -0700

    Stored values

---
+++ script.js
@@ -0,0 +1,35 @@
+// From Netflix
+
+
+let movieTitle="Space Force,";
+let movieTitle1="The Pirates-The last Royal Treasure,";
+let movieTitle2="Just Like Heaven,";
+let movieTitle3="Cosbra Kai,";
+let movieTitle4="Gunpowder.";
+let movieTitle5="Outside The Wire,";
+let movieTitle6="Inception,";
+let movieTitle7="Sherlock Holmes,";
+let movieTitle8="Spectral,"
+let movieTitle9="Saving Private Ryan.";
+let movieTitle10="Army of Thieves,";
+let movieTitle11="The Do-Over,";
+let movieTitle12="Game Over, Man!,";
+let movieTitle13="The Wrong Missy,";
+let movieTitle14="I Now Pronounce You Chuck & Larry,";
+let movieTitle15="Altered Carbon,";
+let movieTitle16="Lost in Space,";
+let movieTitle17="Space Sweepers,";
+let movieTitle18="Blade Trinity,";
+let movieTitle19="Battleship.";
+
+
+document.write(`Popular Comedy Movies: ${movieTitle}
+${movieTitle1} ${movieTitle2} ${movieTitle3} ${movieTitle4}`);
+
+document.write(`  Popular Action Movies: ${movieTitle5} ${movieTitle6} ${movieTitle7} ${movieTitle8} ${movieTitle9}`);
+
+document.write(`  These movies are currently trending: ${movieTitle10} 
+${movieTitle11} ${movieTitle12} ${movieTitle13} ${movieTitle14} ${movieTitle15} ${movieTitle16} ${movieTitle17} ${movieTitle18} ${movieTitle19}`);
+
+ 
+
